9fedc8238abee15c6429b45261829e34fc4317d1
EtSH-C2H6S <2130679254@qq.com>
2026-09-12 19:56:12 +0800

    给收发器加了tooltip

--- kubejs/client_scripts/client_common.js
+++ kubejs/client_scripts/client_common.js
@@ -2,4 +2,5 @@ ItemEvents.tooltip(event => {
     event.add("thermal:ruby_ore","由镭射钻挖掘，不会自然生成")
     event.add("thermal:deepslate_ruby_ore","由镭射钻挖掘，不会自然生成")
     event.add("kubejs:wireless_connect_broken",Text.yellow("在天境的小行星实验室被发现"))
+    event.add("ae2additions:me_wireless_transceiver",Text.yellow("这个设备存在崩档bug，故已被禁用。如果你清楚崩溃触发条件并且很想用这个，请自行作弊获取，且不要将崩溃反馈给制作组"))
 })
